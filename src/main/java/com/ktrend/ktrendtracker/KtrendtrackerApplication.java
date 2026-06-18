0f6b06a14b01094b6fbeac5f1f83166d10e442d2
kang <jwon70295@gmail.com>
2026-06-18 19:54:23 +0900

    프로젝트 초기 설정

---
+++ src/main/java/com/ktrend/ktrendtracker/KtrendtrackerApplication.java
@@ -0,0 +1,13 @@
+package com.ktrend.ktrendtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtrendtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtrendtrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ktrend/ktrendtracker/KtrendtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktrend.ktrendtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtrendtrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
